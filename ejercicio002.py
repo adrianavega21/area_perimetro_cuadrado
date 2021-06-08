5d84168a1ddb74c7d55ce2bc1a2ea41e7bf67bad
adrianavega21 <85266547+adrianavega21@users.noreply.github.com>
2021-06-07 21:02:25 -0500

    Area y perimetro del cuadrado
    Ejercicio del área y perímetro del cuadrado.

---
+++ ejercicio002.py
@@ -0,0 +1,11 @@
+##CALCULAR EL AREA Y PERIMETRO DEL CUADRADO UTILIZANDO NUMEROS FLOTANTES 
+print('AREA Y PERIMETRO DEL CUADRADO')
+num_lado= float (input('Hola! Escribe un lado del cuadrado: \n'))
+area=num_lado*2
+perimetro=num_lado*4
+print ('El perimetro del cuadrado es:')
+print (perimetro)
+print ('El area del cuadrado es:')
+print (area)
+
+
